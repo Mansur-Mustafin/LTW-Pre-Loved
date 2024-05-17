0d6d8547c6de8563116c06da2ab0d897b9bd880f
Mansur <mustafin.mansur02@gmail.com>
2024-05-17 04:03:30 +0100

    fixed bug with chat

--- actions/action_check_new_messages.php
+++ actions/action_check_new_messages.php
@@ -17,14 +17,16 @@ $db = getDatabaseConnection();
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $data = json_decode(file_get_contents('php://input'), true);
     if (!isset($data['user_id']) ||
+        !isset($data['item_id']) ||
         !isset($data['last_message_id'])) {
         http_response_code(400);
     }
 
     $user_id = intval($data['user_id']);
     $last_message_id = intval($data['last_message_id']);
+    $item_id = intval($data['item_id']);
 
-    $messages = getNewMessagesByUserIdAndMessageId($db, $user_id, $last_message_id);
+    $messages = getNewMessagesByUserIdAndMessageId($db, $user_id, $last_message_id, $item_id);
 
     print json_encode($messages);
 }
--- js/chats.js
+++ js/chats.js
@@ -1,6 +1,10 @@
 let last_message_id = document.getElementById("last_message_id").value
 let user_id = document.getElementById("current_user_id").value
-setInterval(checkNewMessages, 2000)
+let current_item_id = document.getElementById("current_item_id").value
+
+setInterval(checkNewMessages, 1000)
+
+console.log(current_item_id)
 
 function checkNewMessages() {
     fetch("../actions/action_check_new_messages.php", {
@@ -9,13 +13,12 @@ function checkNewMessages() {
             'Content-Type': 'application/json',
         },
         body: JSON.stringify(
-            {last_message_id:last_message_id, user_id: user_id}
+            {last_message_id:last_message_id, user_id: user_id, item_id: current_item_id}
         ),
     })
         .then(r =>  r.json().then(data => ({status: r.status, body: data})))
         .then(obj => {
             if(obj.status == 200){
-                console.log(obj.body);
                 if(obj.body.length > 0)
                     window.location.reload()
             }
